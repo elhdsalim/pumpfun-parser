f39192df8c6620b964b2e7a9413731b9d649bd17
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-12-28 21:58:55 +0100

    Add Pumpfun SELL instruction handler

---
+++ src/classes/handlers/SellHandler.ts
@@ -0,0 +1,25 @@
+import { BufferReader } from "../../classes/BufferReader";
+import { DISCRIMINATORS } from "../../constants";
+import { InstructionHandler, ParsedInstruction } from "../../types";
+
+export class SellHandler implements InstructionHandler {
+    private readonly discriminator: string;
+
+    constructor() {
+        this.discriminator = DISCRIMINATORS.SELL;
+    }
+
+    public getDiscriminator(): string {
+        return this.discriminator;
+    }
+
+    public handle(ix: ParsedInstruction): void {
+        console.log("SELL", {
+            amount: BufferReader.u64(ix.data, 8),
+            minSol: BufferReader.u64(ix.data, 16),
+            user: ix.accounts[6],
+            mint: ix.accounts[2],
+            bondingCurve: ix.accounts[3],
+        });
+    }
+}
