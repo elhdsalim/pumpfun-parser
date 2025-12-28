e7889ab4cbd3346644e2040a15ae247b756688e2
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-12-28 22:01:20 +0100

    Initialize Pumpfun transaction parsing

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,45 @@
-import { Monitor } from "./GRPCMonitor"
-import 'dotenv/config';
+import bs58 from "bs58";
+import { Connection } from "@solana/web3.js";
+import { PumpFunParser } from "./classes/PumpfunParser";
+import { BuyHandler } from "./classes/handlers/BuyHandler";
+import { SellHandler } from "./classes/handlers/SellHandler";
+import 'dotenv/config'
 
-const TARGET_ADDRESS = '6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P' // PUMPFUN addres
+async function main() {
+    const conn = new Connection(process.env.RPC_URL!);
 
-const listener = new Monitor(process.env.GRPC_URL!, TARGET_ADDRESS)
-listener.start()
+    const tx = await conn.getTransaction("4PC97hmk25zK3ZxxUWhVZ4p811mWcQde9yyMdwodg9KX7Ev42BCeAWPbSz9asEocRXWb1iuVoJGjmbbwNeAtFZu2", {
+        maxSupportedTransactionVersion: 0
+    });
+
+    if (!tx) return;
+
+    const keys = tx.transaction.message.staticAccountKeys.map(k =>
+        k.toBase58()
+    );
+
+    const instructions = tx.transaction.message.compiledInstructions;
+
+    const parser = new PumpFunParser([
+        new BuyHandler(),
+        new SellHandler(),
+    ]);
+
+    for (const ix of instructions) {
+        if (keys[ix.programIdIndex] !== "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P") continue;
+
+        const rawData =
+            typeof ix.data === "string"
+                ? Buffer.from(bs58.decode(ix.data))
+                : Buffer.from(ix.data);
+
+        const accountIndexes = ix.accountKeyIndexes
+
+        parser.parse({
+            data: rawData,
+            accounts: accountIndexes.map(i => keys[i]),
+        });
+    }
+}
+
+main();
