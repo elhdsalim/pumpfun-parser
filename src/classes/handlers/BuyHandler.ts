e545fd4ff53b296d5a41fe833fbb9c336ac0bcbc
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-12-28 21:58:37 +0100

    Add Pumpfun BUY instruction handler

---
+++ src/classes/handlers/BuyHandler.ts
@@ -0,0 +1,25 @@
+import { BufferReader } from "../../classes/BufferReader";
+import { DISCRIMINATORS } from "../../constants";
+import { InstructionHandler, ParsedInstruction } from "../../types";
+
+export class BuyHandler implements InstructionHandler {
+    private readonly discriminator: string;
+
+    constructor() {
+        this.discriminator = DISCRIMINATORS.BUY;
+    }
+
+    public getDiscriminator(): string {
+        return this.discriminator;
+    }
+
+    public handle(ix: ParsedInstruction): void {
+        console.log("BUY", {
+            amount: BufferReader.u64(ix.data, 8),
+            maxSol: BufferReader.u64(ix.data, 16),
+            user: ix.accounts[6],
+            mint: ix.accounts[2],
+            bondingCurve: ix.accounts[3],
+        });
+    }
+}
