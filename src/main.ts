c8feda5d2b04f47e81a237f8b4f10e9fdc000b55
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-12-31 20:14:35 +0100

    Add callback on start() method

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,52 @@
+import "dotenv/config";
 import bs58 from "bs58";
-import { Connection } from "@solana/web3.js";
+import { Monitor } from "./Monitor";
 import { PumpFunParser } from "./classes/PumpfunParser";
 import { BuyHandler } from "./classes/handlers/BuyHandler";
 import { SellHandler } from "./classes/handlers/SellHandler";
-import 'dotenv/config'
+import { PUMPFUN_PROGRAM } from "./constants";
 
-async function main() {
-    const conn = new Connection(process.env.RPC_URL!);
+const GRPC_URL = process.env.GRPC_URL!;
 
-    const tx = await conn.getTransaction("4PC97hmk25zK3ZxxUWhVZ4p811mWcQde9yyMdwodg9KX7Ev42BCeAWPbSz9asEocRXWb1iuVoJGjmbbwNeAtFZu2", {
-        maxSupportedTransactionVersion: 0
-    });
-
-    if (!tx) return;
-
-    const keys = tx.transaction.message.staticAccountKeys.map(k =>
-        k.toBase58()
-    );
-
-    const instructions = tx.transaction.message.compiledInstructions;
-
-    const parser = new PumpFunParser([
-        new BuyHandler(),
-        new SellHandler(),
-    ]);
-
-    for (const ix of instructions) {
-        if (keys[ix.programIdIndex] !== "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P") continue;
-
-        const rawData =
-            typeof ix.data === "string"
-                ? Buffer.from(bs58.decode(ix.data))
-                : Buffer.from(ix.data);
-
-        const accountIndexes = ix.accountKeyIndexes
-
-        parser.parse({
-            data: rawData,
-            accounts: accountIndexes.map(i => keys[i]),
-        });
-    }
+function decodeKeys(message: any): string[] {
+    const rawKeys: Buffer[] = message.staticAccountKeys ?? message.accountKeys ?? [];
+    return rawKeys.map((k) => bs58.encode(Buffer.from(k)));
 }
 
-main();
+function decodeData(ix: any): Buffer | null {
+    if (!ix?.data) return null;
+    return typeof ix.data === "string" ? Buffer.from(ix.data, "base64") : Buffer.from(ix.data);
+}
+
+async function main() {
+    console.log("Listening for Pump.fun transactions...\n");
+
+    const parser = new PumpFunParser([new BuyHandler(), new SellHandler()]);
+    const monitor = new Monitor(GRPC_URL, PUMPFUN_PROGRAM);
+
+    await monitor.start((update) => {
+        const tx = update?.transaction?.transaction?.transaction;
+        const message = tx?.message;
+        if (!message) return;
+
+        const keys = decodeKeys(message);
+        if (keys.length === 0) return;
+
+        const instructions = message.instructions ?? [];
+        for (const ix of instructions) {
+            if (keys[ix.programIdIndex] !== PUMPFUN_PROGRAM) continue;
+
+            const rawData = decodeData(ix);
+            if (!rawData) continue;
+
+            const accountIndexes: number[] = Array.from(ix.accounts ?? []);
+
+            parser.parse({
+                data: rawData,
+                accounts: accountIndexes.map((i) => keys[i]),
+            });
+        }
+    });
+}
+
+main().catch(console.error);
